139779f2d5fd197f68e039c27444e11af19174ed
Jake Garcia <bballwiz4@gmail.com>
2019-02-21 16:19:46 -0800

    hamburger dropdown added in navbar and columns with info on the home page

--- Website_Projects/Evzin_Website/client/src/App.vue
+++ Website_Projects/Evzin_Website/client/src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="app">
-      <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">Evzin</a>
-      </nav>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="/">Evzin</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse"          id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span   class="sr-only">(current)</span></a>
+      </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Evzin</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Catering</a>
+        </li>
+      </ul>
+    </div>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
   body {
-    height: 100%;
+    width: 100%;
+    height: auto;
+    background-image: url('../public/media/img/chios_pattern.jpg');
+    background-attachment: fixed;
   }
 </style>
--- Website_Projects/Evzin_Website/client/src/views/Home.vue
+++ Website_Projects/Evzin_Website/client/src/views/Home.vue
@@ -1,44 +1,35 @@
 <template>
-  <div class="container-fluid mt-5">
+  <div class="container-fluid mt-4">
     <div class="row text-center">
       <div class="col">
-        <h1>Evzin</h1>
+        <h1 id="title">Evzin</h1>
       </div>
     </div>
-    <div class="row text-center mt-4">
-      <div class="row">
-        <div class="col-sm-4">
-          <h3>
-            John Tsoutis
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nisi inventore cumque veritatis deserunt doloremque facere sunt suscipit? Similique, omnis qui inventore a praesentium voluptatem expedita. Fuga fugiat quisquam aut?
-          </p>
-          <router-link to="./About">
-            <button type="button" class="btn btn-dark">
-              About Evzin
-            </button>
-          </router-link>
-        </div>
-        <div class="col-sm-4">
-          <h3>
-            Private Events
-          </h3>
-          <p>
-            We offer services for any kind of event inside and outside of the restaurant. Whether it is a private birthday in the bar with family style starters and entrees, or a wedding that wants amazing mediterranean food. If you would like to see our catering menu and pricing
-          </p>
-          <router-link to="./Catering">
-            <button type="button" class="btn btn-dark">Menu and Pricing</button>
-          </router-link>
-        </div>
-        <div class="col-sm-4">
-          <h3>Order Online</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem sequi vero explicabo repudiandae voluptatem id adipisci dignissimos nihil error voluptas quisquam minus, enim provident impedit distinctio a quae? Ad, numquam.
-          </p>
-          <router-link to="./order_Online">
-            <button type="button" class="btn btn-dark">Order Online</button>
-          </router-link>
+    <div class="container">
+      <div class="row text-center mt-4">
+        <div class="row">
+          <div class="col">
+            <h3>
+              John Tsoutis
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nisi inventore cumque veritatis deserunt doloremque facere sunt suscipit? Similique, omnis qui inventore a praesentium voluptatem expedita. Fuga fugiat quisquam aut?
+            </p>
+          </div>
+          <div class="col">
+            <h3>
+              Private Events
+            </h3>
+            <p>
+              We offer services for any kind of event inside and outside of the restaurant. Whether it is a private birthday in the bar with family style starters and entrees, or a wedding that wants amazing mediterranean food. If you would like to see our catering menu and pricing
+            </p>
+          </div>
+          <div class="col">
+            <h3>Order Online</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem sequi vero explicabo repudiandae voluptatem id adipisci dignissimos nihil error voluptas quisquam minus, enim provident impedit distinctio a quae? Ad, numquam.
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -52,3 +43,18 @@ export default {
   
 };
 </script>
+<style>
+  #title {
+    background-color: white;
+}
+
+  .col {
+  background-color: white;
+  margin: 2px;
+}
+
+  .container {
+    align-content: center;
+}
+</style>
+
