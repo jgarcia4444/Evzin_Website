602ba0153968824b67aea06d54556b80c56c04cf
Jake Garcia <bballwiz4@gmail.com>
2019-03-12 15:52:55 -0700

    installed remaining dependencies on the backend to validate, store, and transfer data from the frontend form to the MongoDb

---
+++ Website_Projects/Evzin_Website/server/README.md
@@ -0,0 +1,4 @@
+BACKEND TO DO:
+[] - create a connection to mongoDb
+[] - create a person object to store info
+[] - create schema to validate info entering Db and check for duplicates
--- Website_Projects/Evzin_Website/server/app.js
+++ Website_Projects/Evzin_Website/server/app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const joi = require('joi');
+const monk = require('monk');
+
 
 
 const app = express()
@@ -7,9 +12,9 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
-})
+});
 
 app.get ('/', (req, res) => {
     // 
     res.json('Hello World');
-})
+});
